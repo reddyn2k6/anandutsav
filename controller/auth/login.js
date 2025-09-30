2b7f932971a12bd6324613c5cbc4d6121076c9e3
reddyn2k6 <nihalreddy.vanga@gmail.com>
2025-09-30 18:53:15 +0530

    Update login.js

--- controller/auth/login.js
+++ controller/auth/login.js
@@ -56,7 +56,9 @@ if(!userOtp) {
     // Check OTP
     try{
         
-
+if(userOtp!==u.Otp) {
+return res.status(400).json((success: false, msg: "Invalid Otp"));
+}
     // Check expiry
     if (Date.now() > u.OtpExpireAt) {
       return res.status(400).json({success: false, msg: "OTP has expired" });
